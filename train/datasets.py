d1e7f2b0ee6848146869bf1222ee5294a1a4d618
mountinyy <sankm@postech.ac.kr>
2023-03-20 19:39:48 +0900

    feat: Add TLDRDataset class

--- train/datasets.py
+++ train/datasets.py
@@ -1,12 +1,20 @@
 from torch.utils.data import Dataset
 
 
-class BlobDatset(Dataset):
-    def __init__(self, path: str, usage: str):
-        pass
+class TLDRDataset(Dataset):
+    def __init__(self, path: str, tokenizer, usage: str):
+        with open(path, "r") as f:
+            data = [eval(line.replace("null", "None")) for line in f.readlines()]
+        self.contexts = [
+            tokenizer(item["post"], padding="max_length", truncation=True, max_length=1024) for item in data
+        ]
+        self.summaries = [
+            tokenizer(item["summary"], padding="max_length", truncation=True, max_length=1024)["input_ids"]
+            for item in data
+        ]
 
     def __len__(self):
-        pass
+        return len(self.summaries)
 
     def __getitem__(self, idx):
-        pass
+        return {"context": self.contexts[idx], "summary": self.summaries[idx]}
