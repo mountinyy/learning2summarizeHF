7d0ba85ad4a12be922e5421b54ae290278a397e6
mountinyy <jtlsan@naver.com>
2023-03-29 00:19:30 +0900

    fix: Fix gpu usage issues and add gradient accumulation

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
+import argparse
 import random
 
 import numpy as np
 import torch
 from omegaconf import OmegaConf
 
-# from train.actor_train import train_actor
+from train.actor_train import train_actor
 from train.critic_train import train_reward
-
-# from train.actor import train_actor
 from train.datasets import AnthropicDataset
 
-"""
+
 def get_args():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
-        "--model_name",
+        "--run",
         required=True,
-        default="facebook/bart-base",
-        choices=["facebook/bart-base"],
-        help="pretrained model name form huggingface",
+        choices=["sft", "rm", "rl", "all"],
+        help="Choose what kind of model to train",
     )
 
     return arg_parser.parse_args()
-"""
 
 
 def set_seed(seed):
@@ -33,7 +30,7 @@ def set_seed(seed):
 
 
 if __name__ == "__main__":
-    # args = get_args()
+    args = get_args()
     conf = OmegaConf.load("./config.yaml")
     set_seed(conf.common.seed)
 
@@ -41,7 +38,15 @@ if __name__ == "__main__":
     download_dataset = AnthropicDataset(conf)
     download_dataset.save_dataset()
 
-    # SFT
-    # train_actor(conf)
-    # RM
-    train_reward(conf)
+    if args.run == "sft":
+        print("=" * 50 + "\nTraining SFT\n" + "=" * 50)
+        train_actor(conf)
+    elif args.run == "rm":
+        print("=" * 50 + "\nTraining RM\n" + "=" * 50)
+        train_reward(conf)
+    elif args.run == "rl":
+        print("=" * 50 + "\nTraining RL\n" + "=" * 50)
+        pass
+    elif args.run == "all":
+        print("=" * 50 + "\nTraining ALL\n" + "=" * 50)
+        pass
--- train/actor_train.py
+++ train/actor_train.py
@@ -22,16 +22,20 @@ def train_actor(conf):
     valid_dataset = SFTDataset(
         os.path.join(conf.dataset.save_path, conf.dataset.sft_path), conf.common.data_limit, is_valid=True
     )
+    import pdb
+
+    pdb.set_trace()
     # DataLoader 설정
     train_dataloader = DataLoader(train_dataset, batch_size=conf.common.batch_size, collate_fn=collate_fn)
     valid_dataloader = DataLoader(valid_dataset, batch_size=conf.common.batch_size, collate_fn=collate_fn)
 
     # wandb 설정
-    wandb.login()
-    if conf.wandb.run_name:
-        wandb.init(project=conf.wandb.project_name, name=conf.sft.model_name + "-" + conf.wandb.run_name)
-    else:
-        wandb.init(project=conf.wandb.project_name, name=conf.sft.model_name)
+    if conf.wandb.use:
+        wandb.login()
+        if conf.wandb.run_name:
+            wandb.init(project=conf.wandb.project_name, name=conf.sft.model_name + "-" + conf.wandb.run_name)
+        else:
+            wandb.init(project=conf.wandb.project_name, name=conf.sft.model_name)
 
     # Model 설정 (model, tokenizer, (config))
     model = Actor(conf)
@@ -84,6 +88,7 @@ def train_actor(conf):
             total=int(train_dataset.len / conf.common.batch_size),
             desc=f"train {epoch} epochs",
         )
+        model.zero_grad()
         for i, data in pbar:
             tokenized_data = model.tokenizer(data, padding=True, truncation=True, return_tensors="pt")
             states = tokenized_data["input_ids"]
@@ -97,50 +102,25 @@ def train_actor(conf):
             loss = loss_fn(
                 total_action_probs.view(output_states.size(0) * output_states.size(1), -1), output_states.view(-1)
             )
-            optimizer.zero_grad()
+            loss = loss / conf.sft.gradient_accumulation
             loss.backward()
-            optimizer.step()
-            scheduler.step()
 
             loss_value = loss.item()
             total_loss += loss_value
-            wandb.log({"train/loss": loss_value})
-            pbar.set_postfix_str(f"loss {loss_value}")
+            if conf.wandb.use:
+                wandb.log({"train/loss": loss_value})
+            pbar.set_postfix_str(f"loss: {loss_value}")
+            if (i + 1) % conf.sft.gradient_accumulation == 0:
+                optimizer.step()
+                scheduler.step()
+                model.zero_grad()
+                optimizer.zero_grad()
+
+        run_validation(conf, model, valid_dataset, valid_dataloader, loss_fn, epoch, device)
 
         mean_loss = total_loss / (train_dataset.len / conf.common.batch_size)
-        wandb.log({"train/mean_loss": mean_loss})
-
-        # Valid
-        torch.cuda.empty_cache()
-        model.eval()
-        pbar = tqdm(
-            enumerate(valid_dataloader),
-            total=int(valid_dataset.len / conf.common.batch_size),
-            desc=f"valid {epoch} epochs",
-        )
-        total_loss = 0
-        with torch.no_grad():
-            for i, data in pbar:
-                tokenized_data = model.tokenizer(data, padding=True, truncation=True, return_tensors="pt")
-                states = tokenized_data["input_ids"]
-                states_mask = tokenized_data["attention_mask"]
-
-                input_states = states[:, :-1].to(device)
-                input_states_mask = states_mask[:, :-1].to(device)
-                output_states = states[:, 1:].to(device)
-
-                total_action_probs = model(input_states, input_states_mask)
-                loss = loss_fn(
-                    total_action_probs.view(output_states.size(0) * output_states.size(1), -1), output_states.view(-1)
-                )
-
-                loss_value = loss.item()
-                total_loss += loss_value
-                wandb.log({"valid/loss": loss_value})
-                pbar.set_postfix_str(f"loss {loss_value}")
-
-        mean_loss = total_loss / (valid_dataset.len / conf.common.batch_size)
-        wandb.log({"valid/mean_loss": mean_loss})
+        if conf.wandb.use:
+            wandb.log({"train/mean_loss": mean_loss})
 
         # save checkpoint
         if epoch % conf.common.checkpoint_epoch == 0:
@@ -163,3 +143,38 @@ def load_checkpoint(model, optimizer, checkpoint_path, name, device):
                 state[k] = v.to(device)
 
     return model, optimizer, epoch
+
+
+def run_validation(conf, model, valid_dataset, valid_dataloader, loss_fn, epoch, device):
+    torch.cuda.empty_cache()
+    model.eval()
+    pbar = tqdm(
+        enumerate(valid_dataloader),
+        total=int(valid_dataset.len / conf.common.batch_size),
+        desc=f"valid {epoch} epochs",
+    )
+    total_loss = 0
+    with torch.no_grad():
+        for i, data in pbar:
+            tokenized_data = model.tokenizer(data, padding=True, truncation=True, return_tensors="pt")
+            states = tokenized_data["input_ids"]
+            states_mask = tokenized_data["attention_mask"]
+
+            input_states = states[:, :-1].to(device)
+            input_states_mask = states_mask[:, :-1].to(device)
+            output_states = states[:, 1:].to(device)
+
+            total_action_probs = model(input_states, input_states_mask)
+            loss = loss_fn(
+                total_action_probs.view(output_states.size(0) * output_states.size(1), -1), output_states.view(-1)
+            )
+
+            loss_value = loss.item()
+            total_loss += loss_value
+            if conf.wandb.use:
+                wandb.log({"valid/loss": loss_value})
+            pbar.set_postfix_str(f"loss {loss_value}")
+
+    mean_loss = total_loss / (valid_dataset.len / conf.common.batch_size)
+    if conf.wandb.use:
+        wandb.log({"valid/mean_loss": mean_loss})
